43c0751ef5d5cec9037bd699865977dee69716e0
feng qin <qinfeng0152@gmail.com>
2014-12-31 16:02:20 +0100

    ajout valeur "location" for afterSave "Restaurant"

--- cloud/main.js
+++ cloud/main.js
@@ -6,3 +6,29 @@ Parse.Cloud.beforeSave("Photo", function(req, res) {
     return saveImage(req,res);
 });
 
+Parse.Cloud.afterSave("Restaurant",function(req){
+    var resto = req.object;
+    var address = resto.get("address");
+    var city = resto.get("city");
+    var postalCode = resto.get("postalCode");
+    var queryString = address+", "+postalCode+", "+city+", FR";
+
+    Parse.Cloud.httpRequest({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json',
+        params: {
+            address : queryString,
+            key : 'AIzaSyA9odZ-917yCv0JZWxpNa8TYBRk7HDgg_w'
+        },
+        success: function(httpResponse) {
+            var geoLocation = httpResponse.data.results[0].geometry.location;
+            var point = new Parse.GeoPoint({latitude: geoLocation.lat, longitude: geoLocation.lng});
+            resto.set("location",point);
+            resto.save();
+            console.log("saved location");
+        },
+        error: function(httpResponse) {
+            console.error('Request failed with response code ' + httpResponse.status);
+        }
+    });
+
+})
